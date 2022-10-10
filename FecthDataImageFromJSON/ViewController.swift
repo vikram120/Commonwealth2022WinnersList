e4a93f3e91fa4936e9ebe5185262b673cb22898e
vikram <vikramkunwar1404@gmail.com>
2022-10-10 13:06:59 +0530

    Initial Commit

---
+++ FecthDataImageFromJSON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FecthDataImageFromJSON
+//
+//  Created by Vikram Kunwar on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
